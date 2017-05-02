2cc2fc7852f5a8707b6b9ec3dc141cd796c20f47
Randy <randylam87@gmail.com>
2017-05-02 00:52:30 -0700

    added functions to reduce duplicate code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,33 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
-var player1 = false;
-var player2 = false;
-var wins = 0;
-var losses = 0;
 var player1ref = database.ref('/player1');
 var player2ref = database.ref('/player2');
+var userStatus = 'spectator';
+var user;
+var userUid;
+var signedIn = false;
+
+
+
+//debug
+function debug () {
+  console.log('user: ' + user);
+  console.log('userUid: ' + userUid);
+  console.log(signedIn);
+  console.log(userStatus);
+}
 
 //Starting game
 function startGame() {
-  if (database.ref('/player1') === true && database.ref('/player2') === true) {
-    appendGame('.player1');
-    appendGame('.player2');
-  }
+  player1ref.update({
+    status: 'open'
+  })
+  player2ref.update({
+    status: 'open'
+  })
 };
+startGame();
 
 function appendGame(target) {
   var rock = $('<div class="rock">Rock</div>')
@@ -37,58 +50,143 @@ function appendGame(target) {
   $(target).append(choices);
 }
 
+function handleErrors (errors) {
+  console.log(errors);
+}
+
+function changeLogInBtn (firebaseUser){
+  if (firebaseUser) {
+    $('.login').addClass('hide');
+    $('.logout').removeClass('hide');
+    $('.message-box').html('Welcome ' + firebaseUser.displayName)
+    signedIn = true;
+  } else {
+    $('.login').removeClass('hide');
+    $('.logout').addClass('hide');
+    $('.message-box').html('Please log in')
+  }
+}
+
+function playerOne () {
+player1ref.on('value', function(snapshot) {
+  if (snapshot.val().uid == userUid && userStatus === 'player1') {
+    $('.message-box').append('<div class="player">You are Player 1</div>');
+    player1ref.update({
+      status: 'closed'
+    })
+  } else if (snapshot.val().status == 'open' && userStatus === 'spectator' && signedIn === true) {
+    player1ref.update({
+      name: user.displayName,
+      uid: userUid,
+      status: 'closed'
+    }).then(function(){
+      console.log('current user uid:  ' + userUid);
+      console.log('player1 uid:  ' + snapshot.val().uid);
+      userStatus = 'player1';
+      player2ref.update({
+      player1: 'true'
+    });
+    })    
+  }
+})
+}
+
+function playerTwo () {
+player2ref.on('value', function(snapshot) {
+  if (snapshot.val().uid == userUid && userStatus === 'player2') {
+    $('.message-box').append('<div class="player">You are Player 2</div>');
+    player2ref.update({
+      status: 'closed'
+    })
+  } else if (snapshot.val().status == 'open' && userStatus === 'spectator' && signedIn === true && userStatus !== 'player1') {
+    player2ref.update({
+      name: user.displayName,
+      uid: userUid,
+      status: 'closed'
+    }).then(function(){
+      console.log('current user uid:  ' + userUid);
+      console.log('player1 uid:  ' + snapshot.val().uid);
+      userStatus = 'player1';
+      player1ref.update({
+      player2: 'true'
+    });
+    })    
+  }
+})
+}
+
+
+
+
+
+
 // Click event listener for log in
 $('#play-btn').on('click', function() {
   var name = $('#usr').val();
-  var user = firebase.auth().currentUser;
-  firebase.auth().signInAnonymously().then(function(){
+  firebase.auth().signInAnonymously().then(function() {
+    user = firebase.auth().currentUser;
+    debug();
     firebase.auth().currentUser.updateProfile({
       displayName: name
-    }).then(function(){
+    }).then(function() {
+      userUid = firebase.auth().currentUser.uid;
       firebase.auth().onAuthStateChanged(function(firebaseUser) {
-        if (firebaseUser) {
-          $('.login').addClass('hide');
-          $('.logout').removeClass('hide');
-          $('.message-box').html('Welcome ' + firebaseUser.displayName)
-        } else {
-          $('.login').removeClass('hide');
-          $('.logout').addClass('hide');
-          $('.message-box').html('Please log in')
-        }
+        changeLogInBtn (firebaseUser)
+        playerOne();
+        playerTwo();
       });
-    });
-  })
+    })
+  }).catch(handleErrors())
 });
 
 // Click event listener for log out
 $('.logout').on('click', function() {
+  if (userStatus === 'player1') {
+    player1ref.update({
+      status: 'open'
+    })
+  } else if (userStatus === 'player2') {
+    playe2ref.update({
+      status: 'open'
+    })
+  };
   firebase.auth().currentUser.delete();
+  userStatus = 'spectator';
+  signedIn = false;
 });
 
-player1ref.on('value',function(snapshot){
-  var user = firebase.auth().currentUser.displayName;
-  if(snapshot.val().status == 'open'){
-    console.log('line 79: player1 status is: ' + snapshot.val().status)
 
-    // console.log('snapshot status: ' + snapshot.val().status)
-    // console.log('snapshot name: ' + snapshot.val().name)
-    // snapshot.val().update({
-    //   name: user.displayName
-  }
+
+
+
+
+
+
+
+
+//handlers
+firebase.auth().onAuthStateChanged(function(firebaseUser) {
+  changeLogInBtn (firebaseUser);
 })
 
-// //Player 1 value listener
-// database.ref('/player1').on('value', function(snapshot) {
-//   if (snapshot.val() === null && player2 === false) {
-//     console.log('no player 1 found');
-//     database.ref('/player1').set({
-//       name: auth.currentUser.displayName,
-//       id: auth.currentUser.uid,
-//       wins: wins,
-//       losses: losses
+
+
+
+
+
+
+
+
+
+// player2ref.on('value', function(snapshot) {
+//   var user = firebase.auth().currentUser.displayName;
+//   var userUid = firebase.auth().currentUser.uid
+//   if (snapshot.val().status == 'open' && userUid !== player1ref.val().uid) {
+//     console.log('line 79: player1 status is: ' + snapshot.val().status)
+//     player2ref.update({
+//       name: user,
+//       uid: userUid
 //     })
-//     player1 = true;
-//     console.log('You are Player 1 ' + player1)
 //   }
 // })
 
